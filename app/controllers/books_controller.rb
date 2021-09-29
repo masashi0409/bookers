63be2a8d26ac40fbb23e3d3ab462ee6adf01e85c
ShimanoMasashi <masashi.shimano0409@gmail.com>
2021-09-29 04:57:40 +0000

    新規登録 new 修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -15,7 +16,7 @@ class BooksController < ApplicationController
     @book = Book.new(new_book_params)
     if @book.save
       flash[:notice] = 'Book was successfully created.'
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       @books = Book.all
       flash[:error] = @book.errors.full_messages.count
@@ -44,7 +45,7 @@ class BooksController < ApplicationController
 
   private
   def new_book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
   def book_params
     params.require(:book).permit(:title, :body)
